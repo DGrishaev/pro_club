a97e081d8a6275cc6be82a9b122f919c90720235
DGrishaev <d.grishaev@gmail.com>
2026-03-04 15:05:02 +0300

    чистка кэша

--- python/test_py_module/main.py
+++
@@ -1,8 +0,0 @@
-import my_llm
-while True:
-    request = input("Введите запрос, или end для выхода:  ")
-    if request == "end":
-        break
-    llm_inv = my_llm.llm_invoke(request)
-    print (llm_inv)
-
